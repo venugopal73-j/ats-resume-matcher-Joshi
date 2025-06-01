fcc79b34eb611c70357ed18e75819c816306559e
venugopal73-j <venugopal73@gmail.com>
2025-06-01 16:19:09 +0530

    Add files via upload
    reupload

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-
-def is_null_or_whitespace(s):
-    return s.strip() == ''
-
 import streamlit as st
 from resume_processor import extract_text
 from matcher import get_match_score, analyze_keywords
